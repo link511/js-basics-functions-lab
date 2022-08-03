0021387c34c1316463f032e01cd79a2f1d0c031f
Lincoln <lincoln@taylorlivingco.com>
2022-08-03 20:57:15 +1000

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+    if (distance > 42) {
+        return distance - 42;
+    }
+    else if (distance < 42) {
+        return 42 - distance;
+    }
+}
+
+function distanceFromHqInFeet (distance) {
+   return distanceFromHqInBlocks (distance) * 264;
+}
+
+function distanceTravelledInFeet (start, end) {
+    if (start > end) {
+        return (start - end) * 264;
+    }
+    else if (end > start) {
+        return (end - start) * 264;
+    }
+
+}
+
+function calculatesFarePrice (start, end) {
+
+    if (distanceTravelledInFeet (start, end) <= 400) {
+        return (0);
+    }
+    else if (distanceTravelledInFeet (start, end) > 400 &&  distanceTravelledInFeet (start, end) <= 2000) {
+        return ((distanceTravelledInFeet (start, end)-400) * .02);
+    }
+    else if (distanceTravelledInFeet (start, end) > 2000  && distanceTravelledInFeet (start, end) < 2500) {
+        return (25);
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}    
